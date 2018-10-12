924ff155d605e9b53e586f43c09ab97c43e1a55b
atchuth <avcherukuri@gmail.com>
2018-10-11 20:25:51 -0500

    First commit

---
+++ src/com/assesment/LongestString.java
@@ -0,0 +1,21 @@
+package com.assesment;
+
+public class LongestString {
+	public String longestStringMethod() {
+
+		String s = "The cow jumped over the moon.";
+		String[] word = s.split("hi ");
+		String maxlethWord = "";
+		for (int i = 0; i < word.length; i++) {
+			if (word[i].length() >= maxlethWord.length()) {
+				maxlethWord = word[i];
+			}
+           			}
+		return maxlethWord;
+	}
+
+	public static void main(String[] args) {
+		LongestString ls = new LongestString();
+		System.out.println(ls.longestStringMethod());
+	}
+}
---
+++ test/com/assesment/LongestStringTest.java
@@ -0,0 +1,14 @@
+package com.assesment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LongestStringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
